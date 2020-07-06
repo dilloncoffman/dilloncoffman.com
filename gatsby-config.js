65b89432ee25cbc101259ac7de7475c01ee3a267
Dillon Coffman <tug40394@temple.edu>
2020-07-05 21:46:52 -0400

    Added gatsby-transformer-remark plugin amongst other remark specific plugins for the future to be able to query Markdown files from source file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,16 +16,41 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog-posts`,
+        path: `${__dirname}/content/blog-posts`,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 710,
+            },
+          },
+          {
+            resolve: "gatsby-remark-responsive-iframe",
+          },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-autolink-headers",
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Dillon Coffman`,
+        short_name: `Dillon Coffman`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
