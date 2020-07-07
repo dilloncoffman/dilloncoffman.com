54c53c26d580c1c6a24b349ebde198888d8517aa
Dillon Coffman <tug40394@temple.edu>
2020-07-07 18:22:48 -0400

    Updated content/blog-posts folder dates and markdown file names for page slugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Box, Divider, Heading, Text, Flex, PseudoBox } from "@chakra-ui/core"
-import { graphql } from "gatsby"
+import { Link as GatsbyLink, graphql } from "gatsby"
 import { motion } from "framer-motion"
 
 const MotionCard = motion.custom(PseudoBox)
 
 const IndexPage = ({ data }) => (
   <Layout>
-    {console.log(data)}
     <SEO title="Home" />
     <Heading as="h1" size="2xl">
       Dillon Coffman
@@ -26,33 +25,35 @@ const IndexPage = ({ data }) => (
     </Text>
 
     {data.allMarkdownRemark.edges.map(({ node }) => (
-      <MotionCard
-        key={node.id}
-        bg="white"
-        borderWidth="1px"
-        borderColor="slate.500"
-        rounded="lg"
-        overflow="hidden"
-        _hover={{
-          boxShadow: "0 8px 6px -6px gray",
-          cursor: "pointer",
-        }}
-        whileHover={{ scale: 1.025 }}
-        whileTap={{ scale: 0.975 }}
-        mb={5}
-      >
-        <Box p={6}>
-          <Heading as="h3" size="lg" color="slate.800">
-            {node.frontmatter.title}
-          </Heading>
-          <Flex color="slate.400" fontSize="sm" fontWeight="bold" mb={2}>
-            <Text as="span">{node.timeToRead} minute read</Text>
-            <Divider borderColor="slate.200" orientation="vertical" />
-            <Text as="span">{node.frontmatter.date.toUpperCase()}</Text>
-          </Flex>
-          <Text>{node.excerpt}</Text>
-        </Box>
-      </MotionCard>
+      <GatsbyLink to={node.fields.slug}>
+        <MotionCard
+          key={node.id}
+          bg="white"
+          borderWidth="1px"
+          borderColor="slate.500"
+          rounded="lg"
+          overflow="hidden"
+          _hover={{
+            boxShadow: "0 8px 6px -6px gray",
+            cursor: "pointer",
+          }}
+          whileHover={{ scale: 1.025 }}
+          whileTap={{ scale: 0.975 }}
+          mb={5}
+        >
+          <Box p={6}>
+            <Heading as="h3" size="lg" color="slate.800">
+              {node.frontmatter.title}
+            </Heading>
+            <Flex color="slate.400" fontSize="sm" fontWeight="bold" mb={2}>
+              <Text as="span">{node.timeToRead} minute read</Text>
+              <Divider borderColor="slate.200" orientation="vertical" />
+              <Text as="span">{node.frontmatter.date.toUpperCase()}</Text>
+            </Flex>
+            <Text>{node.excerpt}</Text>
+          </Box>
+        </MotionCard>
+      </GatsbyLink>
     ))}
   </Layout>
 )
@@ -67,6 +68,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
           timeToRead
